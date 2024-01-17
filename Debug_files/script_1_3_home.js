9ddadf5bc4811f2872f0a3ea4a0e68b28dbe144d
vijaya_27 <111644845+vix2704@users.noreply.github.com>
2024-01-18 03:25:12 +0530

    Add files via upload

---
+++ Debug_files/script_1_3_home.js
@@ -0,0 +1,14 @@
+function goBack() {
+    // You can implement redirection logic to navigate back to the previous page
+    console.log("Back to the previous page");
+}
+
+function selectExistingRule() {
+    // Redirect to 1_2_select page
+    window.location.href = 'index_1_3_select.html'; // Adjust the redirection URL as needed
+}
+
+function createNewRule() {
+    // Redirect to 1_2_create page
+    window.location.href = 'index_1_3_create.html'; // Adjust the redirection URL as needed
+}
---
+++ Debug_files/script_1_3_select.js
@@ -0,0 +1,9 @@
+function goBack() {
+    // Redirect to 1_2_home page
+    window.location.href = 'index_1_2_home.html'; // Adjust the redirection URL as needed
+}
+
+function debug() {
+    // Redirect to 1_3_debug page
+    window.location.href = 'index_1_3_debug.html'; // Adjust the redirection URL as needed
+}
