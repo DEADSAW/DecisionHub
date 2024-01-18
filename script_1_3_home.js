deba8ae55449c301d48c2b8c08c9081f9fe58ec8
vijaya_27 <111644845+vix2704@users.noreply.github.com>
2024-01-18 12:52:26 +0530

    Add files via upload

---
+++ script_1_3_home.js
@@ -0,0 +1,20 @@
+function goBack() {
+    // Redirect to homepage
+    console.log("Navigating back to homepage");
+    window.location.href = 'index_1_3_home.html'; // Adjust the redirection URL as needed
+    
+}
+
+function selectExistingStrategy() {
+    // Redirect to 1_3_select page
+    console.log("Navigating to 1_3_select page");
+    window.location.href = 'index_1_3_select.html'; // Adjust the redirection URL as needed
+    
+}
+
+function createNewStrategy() {
+    // Redirect to 1_3_create page
+    console.log("Navigating to 1_3_create page");
+    window.location.href = 'index_1_3_create.html'; // Adjust the redirection URL as needed
+    
+}
